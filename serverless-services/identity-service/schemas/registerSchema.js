79a8956b348bd100df4d805dd2c7a71eccce9b6e
julian <julian.perez@fintak.com.co>
2025-02-22 09:56:11 -0500

    fix: fix in lambdas

--- serverless-services/identity-service/schemas/registerSchema.js
+++ serverless-services/identity-service/schemas/registerSchema.js
@@ -32,7 +32,11 @@ const registerSchema = Joi.object({
       "string.base": "invalid_phone_number",
       "any.required": "phone_number_required",
     }),
-  });
+  role: Joi.string().valid("admin", "user_employee", "user_financial", "user_company").required().messages({
+    "string.base": "invalid_role",
+    "any.required": "role_required",
+  }),
+});
 
 module.exports = {
   registerSchema,
