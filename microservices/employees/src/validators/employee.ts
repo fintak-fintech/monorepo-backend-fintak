a85b1d28a0e9e34f8bec9ca56a1e4648c2978a3c
juanima <juan.urbano@mercadolibre.com.co>
2025-02-23 14:04:26 -0500

    feat: add Joi validation schemas for employee creation, search, and status update

--- microservices/employees/src/validators/employee.ts
+++ microservices/employees/src/validators/employee.ts
@@ -1,11 +1,27 @@
 import Joi from 'joi';
 
 export const employeeSchema = Joi.object({
-    name: Joi.string().required(),
-    position: Joi.string().required(),
+    cognito_sub: Joi.string().required(),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    identification_number: Joi.string().required(),
+    email: Joi.string().email().required(),
+    position: Joi.string().optional(),
+    depto: Joi.string().optional(),
+    phone: Joi.string().optional(),
+    salary: Joi.number().required(),
+    company_id: Joi.string().required(),
 });
 
 
 export const searchEmployeeSchema = Joi.object({
     id: Joi.string().required(),
 });
+
+export const searchEmployeeIDSchema = Joi.object({
+    identification_number: Joi.string().required(),
+});
+
+export const statusSchema = Joi.object({
+    status: Joi.boolean().required(),
+  });
